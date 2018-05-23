24ebb3516094c5811baf6a7853443ff297e6e26f
David Allysson <davidmoreirainformatica@gmail.com>
2018-05-23 18:04:22 -0300

    removendo máscara reversa

--- public/js/app.js
+++ public/js/app.js
@@ -4,8 +4,8 @@ $(document).ready(function(){
     $('select').select();
 
     $('.date').mask('00/00/0000');
-    $('.rg').mask('000.000.000', {reverse: true});
-    $('.cpf').mask('000.000.000-00', {reverse: true});
+    $('.rg').mask('000.000.000');
+    $('.cpf').mask('000.000.000-00');
     $('.phone').mask('(00) 90000-0000');
 
     $('.datepicker').datepicker({
